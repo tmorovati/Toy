cc1933663b4ef818635dc7d1520e172959d93fba
tmorovati <t.morovati.99@gmail.com>
2021-05-24 22:29:51 -0700

    hejazi's docs added

---
+++ test/test/test/main.c
@@ -0,0 +1,20 @@
+/*
+ * test.c
+ *
+ * Created: 5/21/2021 12:28:54 PM
+ * Author : Tahoura Morovati
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+		return ; 
+    }
+}
+
